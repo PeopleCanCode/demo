a3a9066fe5295a8226e9219b3aa1f3f660ddc771
Muzyka Bartek <muzykabartek@gmail.com>
2014-08-26 07:49:16 +0200

    Add posts to the routes
    p3-t2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :users, only: [:index, :show]
+  resources :posts
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
