45c445a299aaae36fc7fcf4f45c6ddf8637a55d1
Muzyka Bartek <muzykabartek@gmail.com>
2014-08-26 08:40:22 +0200

    Add posts deleting
    p3-t6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -19,7 +20,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id]) # NOT NEEDED ANYMORE MOVED TO BEFORE ACTION
   end
 
   def index
@@ -27,12 +28,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id]) # NOT NEEDED ANYMORE MOVED TO BEFORE ACTION
     redirect_to posts_path unless @post.user == current_user
   end
 
   def update
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id]) # NOT NEEDED ANYMORE MOVED TO BEFORE ACTION
     
     if @post.user == current_user
       if @post.update_attributes(allowed_params)
@@ -47,8 +48,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    # @post = Post.find(params[:id]) # NOT NEEDED ANYMORE MOVED TO BEFORE ACTION
+    if @post.user == current_user
+      @post.destroy
+      redirect_to posts_path
+    else
+      redirect_to root_path
+      flash[:danger] = "You can't do this"
+    end
+  end
+
   private
     def allowed_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
     end
 end
