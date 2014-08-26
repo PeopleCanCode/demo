3fed35b727baca3001e11fd73dd1ad233cda038b
Muzyka Bartek <muzykabartek@gmail.com>
2014-08-26 08:20:34 +0200

    Add form for posts
    p3-t4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.all
+  before_action :authenticate_user!, except: [:show, :index]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(allowed_params)
+
+    if @post.save
+      flash[:success] = "Created new post"
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
     @post = Post.find(params[:id])
   end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+    def allowed_params
+      params.require(:post).permit(:title, :body)
+    end
 end
